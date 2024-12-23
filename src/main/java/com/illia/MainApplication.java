109e4398207c05682e82084c6b3f1ecdfc0da937
Ilya Ovramets <72743791+ilya-ovramets@users.noreply.github.com>
2024-12-23 10:57:37 +0200

    Add Swager

--- src/main/java/com/illia/MainApplication.java
+++ src/main/java/com/illia/MainApplication.java
@@ -27,29 +27,29 @@ public class MainApplication {
     BookRepository bookRepository;
 
     // Run this if app.db.init.enabled = true
-    @Bean
-    @ConditionalOnProperty(prefix = "app", name = "db.init.enabled", havingValue = "true")
-    public CommandLineRunner demoCommandLineRunner() {
-        return args -> {
-
-            System.out.println("Running.....");
-
-            Book b1 = new Book("Book A",
-                    BigDecimal.valueOf(9.99),
-                    LocalDate.of(2023, 8, 31));
-            Book b2 = new Book("Book B",
-                    BigDecimal.valueOf(19.99),
-                    LocalDate.of(2023, 7, 31));
-            Book b3 = new Book("Book C",
-                    BigDecimal.valueOf(29.99),
-                    LocalDate.of(2023, 6, 10));
-            Book b4 = new Book("Book D",
-                    BigDecimal.valueOf(39.99),
-                    LocalDate.of(2023, 5, 5));
-
-            bookRepository.saveAll(List.of(b1, b2, b3, b4));
-
-        };
-    }
+//    @Bean
+//    @ConditionalOnProperty(prefix = "app", name = "db.init.enabled", havingValue = "true")
+//    public CommandLineRunner demoCommandLineRunner() {
+//        return args -> {
+//
+//            System.out.println("Running.....");
+//
+//            Book b1 = new Book("Book A",
+//                    BigDecimal.valueOf(9.99),
+//                    LocalDate.of(2023, 8, 31));
+//            Book b2 = new Book("Book B",
+//                    BigDecimal.valueOf(19.99),
+//                    LocalDate.of(2023, 7, 31));
+//            Book b3 = new Book("Book C",
+//                    BigDecimal.valueOf(29.99),
+//                    LocalDate.of(2023, 6, 10));
+//            Book b4 = new Book("Book D",
+//                    BigDecimal.valueOf(39.99),
+//                    LocalDate.of(2023, 5, 5));
+//
+//            bookRepository.saveAll(List.of(b1, b2, b3, b4));
+//
+//        };
+//    }
 
 }
