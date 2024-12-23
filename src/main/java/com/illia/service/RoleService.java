8c36e0f739f13854ab8bcba195bcf15d3717edb5
Ilya Ovramets <72743791+ilya-ovramets@users.noreply.github.com>
2024-12-23 09:32:02 +0200

    Add role service

---
+++ src/main/java/com/illia/service/RoleService.java
@@ -0,0 +1,26 @@
+package com.illia.service;
+
+
+import com.illia.model.Role;
+import com.illia.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public List<Role> findAll(){return roleRepository.findAll();}
+
+    public Optional<Role> findById(Long id){return roleRepository.findById(id);}
+
+    public Role save(Role role){return roleRepository.save(role);}
+
+    public void deleteById(Long id){roleRepository.deleteById(id);}
+
+}
